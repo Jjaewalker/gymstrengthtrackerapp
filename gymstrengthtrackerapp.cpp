2b8e40829a0d4eeca155e557daadf6d75623dcbc
Jjaewalker <n1354913@myntu.ac.uk>
2025-02-11 14:39:01 +0000

    First start

--- gymstrengthtrackerapp.cpp
+++ gymstrengthtrackerapp.cpp
@@ -2,4 +2,24 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <sqlite3.h>
+#include <iomanip>
+
+using namespace std;
+// Struct to hold workout data 
+struct Workout {
+	string exercise;
+	int sets;
+	int reps;
+	double weight;
+	string date;
+};
+
+class StrengthTracker {
+private:
+	vector<Workout> workouts;// Vector to hold all workouts
+	sqlite3* db; // Database object
+
 
